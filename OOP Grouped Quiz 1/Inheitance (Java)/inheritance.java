1d8d2572ab38c94642b9dfa8fbfebad05ec436f4
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-03-30 00:37:35 +0500

    Updated

--- OOP Grouped Quiz 1/Inheitance (Java)/inheritance.java
+++
@@ -1,9 +0,0 @@
-public class inheritance {
-public static void main(String[] args){
-    SportsCar Car = new SportsCar("Honda", "red", 4, 45, 1, 10000, "2" , "yes", 2);
-    Car.display1();
-    Car.display2();
-}
-}
-
-
