eaf473c0c7638591c5790c10fa025bb47d9274fb
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-05-07 16:38:57 +0500

    updated

--- Curve/curve.cpp
+++ Curve/curve.cpp
@@ -2,13 +2,22 @@
 using namespace std;
 
 class curve{
+    private:
+    enum equation {};
+    // int point [] = {};
+
+    public:
     curve(){};
-    void computePoints (){};
+    virtual void computePoints () = 0;
     friend ostream& operator<<(ostream&, const curve& c){};
 };
 
-class curve : public parametericcurve{
+class parametericcurve : public curve{
+    private:
+    float minT, maxT;
+
+    public:
     parametericcurve (){};
-    void computePoints (){};
+    void computePoints ();
     friend ostream& operator<<(ostream&, const parametericcurve& c){};
 };
