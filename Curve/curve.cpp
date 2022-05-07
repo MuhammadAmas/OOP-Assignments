3195ab0247ae349672645e5b46c84780a84e9a70
Muhammad Amas Waseem <84465512+AmasWaseem@users.noreply.github.com>
2022-05-08 03:47:19 +0500

    Updated

--- Curve/curve.cpp
+++ Curve/curve.cpp
@@ -10,17 +10,34 @@ class curve{
     public:
     curve(){};
     virtual void computePoints () = 0;
-    friend ostream& operator<<(ostream&, const curve& c){};
+    // friend ostream& operator<<(ostream&, const curve& c){};
 };
 
+/*
+Equation is;
+x = t^2 + 2  y = 3t+3 
+*/
 class parametericcurve : public curve{
     private:
     float minT, maxT;
 
     public:
     parametericcurve (){};
-    void computePoints ();
-    friend ostream& operator<<(ostream&, const parametericcurve& c){};
+    void computePoints (){
+        for (int i = 0; i <5; i++){
+            cout << "x"<<i+1 <<"= " << pow(i,2) + 2<<endl;
+        }
+        for (int i = 0; i <5; i++){
+            cout << "y"<<i+1 <<"= "<<3*i + 3<<endl;
+        }        
+    };
+    // friend ostream& operator<<(ostream&, const parametericcurve& c){};
 };
 
+int main(){
+    parametericcurve c1;
+    c1.computePoints();
+    return 0;
+
+};
 
